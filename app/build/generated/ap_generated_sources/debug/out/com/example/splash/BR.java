6e638b0ef70ac3b5d845d8be08c395a97fed3e3b
Melissa Rodríguez Hernández <91748294+MelissaRodriguezHernandez@users.noreply.github.com>
2023-01-11 02:37:06 +0100

    Delete app directory

--- app/build/generated/ap_generated_sources/debug/out/com/example/splash/BR.java
+++
@@ -1,5 +0,0 @@
-package com.example.splash;
-
-public class BR {
-  public static final int _all = 0;
-}
--- app/build/generated/data_binding_trigger/debug/com/example/splash/DataBindingTriggerClass.java
+++
@@ -1,4 +0,0 @@
-package com.example.splash;
-
-@androidx.databinding.BindingBuildInfo
-public class DataBindingTriggerClass {}
